3936d9169a82a6b3526da9631426b02d434e71c2
hyungjin0105 <hyungjin015@handong.ac.kr>
2023-11-24 00:02:13 +0900

    1st commit

---
+++ Git_test_HJK/Git_test_HJK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Git_test_HJK
+//
+//  Created by hyungjin kim on 2023/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
